9b229b45b2d46b6275f5b7177d56cafd5317cf72
Kesavan45 <kesavanp555@gmail.com>
2020-03-16 17:03:59 +0530

    API update

--- src/main/java/com/chainsys/grocery/util/ServiceException.java
+++
@@ -1,9 +0,0 @@
-package com.chainsys.grocery.util;
-
-@SuppressWarnings("serial")
-public class ServiceException extends Exception {
-
-	public ServiceException(String msg, Exception e) {
-		super();
-	}
-}
